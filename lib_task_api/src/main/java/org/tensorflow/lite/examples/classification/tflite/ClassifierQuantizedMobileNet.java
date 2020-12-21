684905b561eeede3f1b44cf070f4c2f3b5c8c602
Rodrigo Mendes <rodrigo.ma146@gmail.com>
2020-12-20 22:36:09 -0300

    First

---
+++ lib_task_api/src/main/java/org/tensorflow/lite/examples/classification/tflite/ClassifierQuantizedMobileNet.java
@@ -0,0 +1,56 @@
+package org.tensorflow.lite.examples.classification.tflite;
+
+import android.app.Activity;
+
+import org.tensorflow.lite.support.common.TensorOperator;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+
+import java.io.IOException;
+
+public class ClassifierQuantizedMobileNet extends Classifier {
+
+    private static final float IMAGE_MEAN = 0.5f;
+    private static final float IMAGE_STD = 0.6f;
+
+    /**
+     * Float model does not need dequantization in the post-processing. Setting mean and std as 0.0f
+     * and 1.0f, repectively, to bypass the normalization.
+     */
+    private static final float PROBABILITY_MEAN = 0.0f;
+
+    private static final float PROBABILITY_STD = 1.0f;
+
+    /**
+     * Initializes a {@code ClassifierFloatCustomNet}.
+     *
+     * @param activity
+     */
+    public ClassifierQuantizedMobileNet(Activity activity, Device device, int numThreads)
+            throws IOException {
+        super(activity, device, numThreads);
+    }
+
+    @Override
+    protected String getModelPath() {
+        // you can download this file from
+        // see build.gradle for where to obtain this file. It should be auto
+        // downloaded into assets.
+        return "mobilenet_quant_f16.tflite";
+    }
+
+    @Override
+    protected String getLabelPath() {
+        return "labels.txt";
+    }
+
+    @Override
+    protected TensorOperator getPreprocessNormalizeOp() {
+        return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
+    }
+
+    @Override
+    protected TensorOperator getPostprocessNormalizeOp() {
+        return new NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD);
+    }
+}
+
